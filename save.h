127ebce4c58ac109f543277d4e24df7a48bb940f
FarellaKamala046 <18223046@std.stei.itb.ac.id>
2024-11-22 23:43:19 +0700

    feat: add save.h

---
+++ save.h
@@ -0,0 +1,76 @@
+#ifndef FILE_NAME_H
+#define FILE_NAME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LEN 100
+
+typedef struct {
+    char name[MAX_LEN];
+    char password[MAX_LEN];
+    int money;
+} User;
+
+typedef struct {
+    char name[MAX_LEN];
+    int price;
+} Barang;
+
+typedef struct {
+    User users[MAX_LEN];
+    int count;
+} StaticUserList;
+
+typedef struct {
+    Barang *items;
+    int count;
+    int capacity;
+} DinamicItemList;
+
+void copyString(char* src, char* dest);
+/*menyalin string dari source ke destination sebagai pengganti strcpy
+  I.S. src merupakan source dan dest merupakan destination dari source
+  F.S. src telah disalin ke dest*/
+
+void initStaticUserList(StaticUserList *list);
+/*fungsi iniStaticUserList berfungsi untuk menginisialisasi
+struktur data StaticUserList, dibuat daftar kosong*/
+
+
+void initDinamicItemList(DinamicItemList *list);
+/*fungsi initDinamicItemList berfungsi untuk menginisialisasi
+struktur data DinamicItemList dengan keterangan mengalokasikan
+memori awal array items dengan kapasitas awal 2 dan count 0*/
+
+void addUser(StaticUserList *list, int money, char*name, char*password);
+/*menambahkan user baru ke dalam StaticUserList
+  I.S. jumlah user kurang dari MAX_LEN, name dan password adalah string yang valid,
+       money merupakan integer positif
+  F.S. user baru dengan nama, password, dan money ditambahkan ke list*/
+
+void addItem(DinamicItemList *list, int price, char*name);
+/*menambahkan barang ke dalam DinamicItemList
+  I.S. capacity awal = 2, name adalah string yang valid dan price adalah integer positif
+  F.S. item baru dengan nama dan harga ditambahkan ke list */
+
+void freeDinamicItemList(DinamicItemList *list);
+/*menghapus memori yang telah dialokasikan secara dinamis untuk DinamicItemList,
+  supaya memorinya tidak terbuang sia-sia dan bisa digunakan lagi*/
+void constructfilepath(char*filepath, char*basepath, char*filename);
+//fungsi constructfilepath berfungsi untuk menggabungkan dua string dan membentuk path file*/
+
+void display(DinamicItemList *itemlist, StaticUserList *userlist);
+/*fungsi untuk menampilkan isi daftar barang dan user ke layar*/
+
+void readline(FILE *file, char *buffer);
+/*fungsi readline berfungsi untuk membaca file ke buffer string*/
+
+void writeToFile(FILE *file, DinamicItemList *itemList, StaticUserList *userList);
+/*fungsi ini berfungsi untuk menyimpan data dari DinamicItemList dan StaticUserList ke file
+ditulis detail barang dan detail user ke file*/
+
+void savefilename(char* filename);
+/*fungsi ini berfungsi untuk save file data*/
+
+#endif
