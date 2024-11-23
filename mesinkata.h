0ddc75a2f3e4ac1f629ba1dfcde3f4af4c52599f
refinormdhn <19623028@std.stei.itb.ac.id>
2024-11-23 10:52:55 +0700

    update : menambahkan deskripsi fungsi startMesinKata pada mesinkata.h

--- mesinkata.h
+++ mesinkata.h
@@ -12,5 +12,8 @@ typedef struct {
 
 // Fungsi untuk memulai mesin kata
 void startMesinKata(MesinKarakter *mk, MesinKata *mK);
+/* Memulai pembacaan kata dari mesin karakter */
+/* I.S. Mesin karakter telah diinisialisasi dan siap digunakan */
+/* F.S. Sebuah kata telah dibaca dari mesin karakter dan disimpan dalam mesin kata (mK). Kata berhenti di akhir baris atau setelah spasi, tergantung pada karakter pembatas */
 
 #endif
