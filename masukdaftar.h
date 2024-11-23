99e36e4077172dfa1e44c9d020d8f841c74cfd8e
refinormdhn <19623028@std.stei.itb.ac.id>
2024-11-23 10:46:34 +0700

    update : menambahkan deskripsi setiap fungsi pada masukdaftar.h

--- masukdaftar.h
+++ masukdaftar.h
@@ -14,12 +14,34 @@ typedef struct {
 
 // Operasi string
 int compareStrings(const char *str1, const char *str2);
+/* Membandingkan dua string str1 dan str2 */
+/* I.S.: str1 dan str2 adalah string yang akan dibandingkan */
+/* F.S.: Mengembalikan nilai integer */
+
 void copyString(char *dest, const char *src);
+/* Menyalin string dari source ke destination sebagai pengganti strcpy */
+/* I.S. src merupakan source dan dest merupakan destination dari source */
+/* F.S. src telah disalin ke dest */
 
 // Fungsi utama
 void registerUser();
+/* Fungsi untuk registrasi pengguna baru */
+/* I.S. Sistem PURRMART berjalan dan siap menerima registrasi */
+/* F.S. Akun baru berhasil dibuat jika username belum terdaftar, atau pesan gagal jika username sudah digunakan */
+
 void login();
+/* Fungsi untuk login ke sistem PURRMART */
+/* I.S. Pengguna belum login, atau ada pengguna lain yang sedang login */
+/* F.S. Pengguna berhasil login jika username dan password cocok, atau pesan gagal jika salah */
+
 void logout();
+/* Fungsi untuk logout dari sistem PURRMART */
+/* I.S. Seorang pengguna sedang login */
+/* F.S. Pengguna berhasil logout dan sistem siap untuk login baru */
+
 void mainMenu();
+/* Fungsi untuk menampilkan menu utama PURRMART */
+/* I.S. Program PURRMART aktif */
+/* F.S. Pengguna memilih opsi dan fungsi terkait dijalankan sesuai pilihan */
 
 #endif
