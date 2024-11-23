df93faee21568d4e7fc2b25862a6178328637cd2
FarellaKamala046 <18223046@std.stei.itb.ac.id>
2024-11-24 02:56:28 +0700

    fix: hapus adt pada save.c

--- save.c
+++ save.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 #include "save.h"
 
-void writeToFile(FILE *file, DinamicItemList *itemList, StaticUserList *userList){
-    if (file != NULL){
-        fprintf(file, "%d\n", itemList->count);
-        for (int i = 0; i<itemList->count; i++){
-            fprintf(file, "%d %s\n", itemList->items[i].price, itemList->items[i].name);
-        }
-
-        fprintf(file, "%d\n", userList->count);
-        for (int i = 0; i<userList->count; i++){
-            fprintf(file, "%d %s %s\n", userList->users[i].money, userList->users[i].name, userList->users[i].password);
-        }
-        printf("Save file berhasil disimpan.\n");
-        fclose(file);
-    } else {
-        printf("File gagal disave.\n");
-    }
-
-}
-
 /*fungsi ini untuk testing dengan add item dan add user, dan melihat apakah kesave atau tidak
 void populateList(DinamicItemList *itemList, StaticUserList *userList){
     addItem(itemList, 100, "pikachu");
