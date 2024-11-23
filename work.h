4bb41b90f629a914a1b4f7f07b5a0727ac3e8461
refinormdhn <19623028@std.stei.itb.ac.id>
2024-11-23 10:36:05 +0700

    update : menambahkan deskripsi setiap fungsi

--- work.h
+++ work.h
@@ -14,21 +14,31 @@ typedef struct {
     int durasi; // dalam detik
 } Job;
 
-// // Deklarasi fungsi Mesin Karakter
-// void startMesinKarakter(MesinKarakter *mk);
-// char getNextChar(MesinKarakter *mk);
-// int isEnd(MesinKarakter *mk);
-
-// // Deklarasi fungsi Mesin Kata
-// void startMesinKata(MesinKarakter *mk, MesinKata *mK);
-
 // Operasi string
 int compareStrings(const char *str1, const char *str2);
+/* Membandingkan dua string str1 dan str2 */
+/* I.S.: str1 dan str2 adalah string yang akan dibandingkan */
+/* F.S.: Mengembalikan nilai integer */
+
 void copyString(char *dest, const char *src);
+/*menyalin string dari source ke destination sebagai pengganti strcpy */
+/* I.S. src merupakan source dan dest merupakan destination dari source */
+/* F.S. src telah disalin ke dest */
 
 // Fungsi utama
 void simulateWork(int durasi);
+/* Mensimulasikan proses kerja dengan durasi tertentu */
+/* I.S.: Parameter durasi diberikan dalam satuan detik */
+/* F.S.: Fungsi akan menunggu selama durasi detik, menunjukkan proses kerja yang sedang berlangsung (misalnya, melalui animasi atau teks) */
+
 void work(int *balance);
+/* Menambahkan penghasilan dari pekerjaan yang dipilih ke saldo pengguna */
+/* I.S.: Pointer balance menunjuk ke saldo pengguna saat ini */
+/* F.S.: Saldo (*balance) bertambah sesuai penghasilan pekerjaan yang telah diselesaikan, setelah */
+
 int main();
+/* Fungsi utama program yang menjalankan simulasi kerja dan memperbarui saldo pengguna */
+/* I.S.: Tidak ada saldo awal yang ditetapkan */
+/* F.S.: Memanggil fungsi work() untuk menambahkan penghasilan ke variabel saldo (balance). Setelah pekerjaan selesai, saldo saat ini ditampilkan dalam format rupiah ke layar */
 
 #endif
