f65714525d39435954d09166b0228e1743b42eef
Chhoff21 <hansjoseph.silitonga@gmail.com>
2024-11-25 19:48:52 +0700

    Create tebak_angka.h
    Signed-off-by: Chhoff21 <hansjoseph.silitonga@gmail.com>

---
+++ tebak_angka.h
@@ -0,0 +1,12 @@
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+
+int generate_random_number();
+// Fungsi untuk men-generate angka random dari antara 0 sampai 99
+
+int CharToInt();
+// Fungsi untuk mengubah inputan user yang bertipe karakter menjadi integer
+
+int Tebak_Angka(int uang);
+// Fungsi utama untuk tebak angka 
+
