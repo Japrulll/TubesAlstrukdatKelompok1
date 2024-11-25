f030f8dccc8fe883152b8ad5b80e64c1831d6746
Japrulll <18223067@std.stei.itb.ac.id>
2024-11-25 15:04:18 +0700

    Add files via upload
    Signed-off-by: Japrulll <18223067@std.stei.itb.ac.id>

---
+++ queuejaprul.h
@@ -0,0 +1,25 @@
+#ifndef QUEUEJAPRUL_H
+#define QUEUEJAPRUL_H
+
+#include "barangdin.h"
+
+#define IDX_UNDEF -1
+#define CAPACITY 100
+
+typedef int IdxType;
+
+typedef struct {
+    Barang *A;
+    int idxHead;
+    int idxTail;
+    int Capacity;
+} Antrian;
+
+void CreateQueue(Antrian *q);
+boolean IsEmptyQ(Antrian q);
+boolean IsFull(Antrian q);
+int LengthQ(Antrian q);
+void enqueue(Antrian *q, Barang val);
+void dequeue(Antrian *q, Barang *val);
+
+#endif
