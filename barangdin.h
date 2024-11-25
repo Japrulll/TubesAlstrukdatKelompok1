f292e9f271881c70c332ee8cbb24a6043e2a15c1
Japrulll <18223067@std.stei.itb.ac.id>
2024-11-25 15:12:43 +0700

    Update barangdin.h
    Signed-off-by: Japrulll <18223067@std.stei.itb.ac.id>

--- barangdin.h
+++ barangdin.h
@@ -2,12 +2,10 @@
 #define __BARANG_DINAMIK__
 
 // Boolean
-#define boolean unsigned char
-#define true 1
-#define false 0
+#include "boolean.h"
+
 #define InitialSize 10
 #define MAX_LEN 100
-#define IDX_UNDEF -1
 
 typedef struct {
   char name[MAX_LEN];
@@ -15,19 +13,13 @@ typedef struct {
 } Barang;
 
 typedef int IdxType;
-typedef Barang ElType;
+
 
 typedef struct {
-    ElType *A;
+    Barang *A;
     int Capacity;
-    int Neff;
+    int Count;
 } BarangDin;
-
-typedef struct {
-	ElType *buffer; 
-	int idxHead;
-	int idxTail;
-} Antrian;
 
 // Array Dinamis
 
@@ -61,7 +53,7 @@ int Length(BarangDin array);
  * Mengembalikan elemen array L yang ke-I (indeks lojik).
  * Prekondisi: array tidak kosong, i di antara 0..Length(array).
  */
-ElType Get(BarangDin array, IdxType i);
+Barang Get(BarangDin array, IdxType i);
 
 /**
  * Fungsi untuk mendapatkan kapasitas yang tersedia.
@@ -73,47 +65,12 @@ int GetMAX_LEN(BarangDin array);
  * Fungsi untuk menambahkan elemen baru di index ke-i
  * Prekondisi: array terdefinisi, i di antara 0..Length(array).
  */
-void InsertAt(BarangDin *array, ElType el, IdxType i);
+void InsertAt(BarangDin *array, Barang el, IdxType i);
 
 /**
  * Fungsi untuk menghapus elemen di index ke-i BarangDin
  * Prekondisi: array terdefinisi, i di antara 0..Length(array).
  */
 void DeleteAt(BarangDin *array, IdxType i);
-
-// QUEUE
-void CreateQueue(Antrian *q);
-
-boolean isEmpty(Antrian q);
-/* Mengirim true jika q kosong: lihat definisi di atas */
-boolean isFull(Antrian q);
-/* Mengirim true jika tabel penampung elemen q sudah penuh */
-/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
-
-int length(Antrian q);
-/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
-
-/* *** Primitif Add/Delete *** */
-void enqueue(Antrian *q, ElType val);
-/* Proses: Menambahkan val pada q dengan aturan FIFO */
-/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
-/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
-
-void dequeue(Antrian *q, ElType *val);
-/* Proses: Menghapus val pada q dengan aturan FIFO */
-/* I.S. q tidak mungkin kosong */
-/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
-        q mungkin kosong */
-
-/* *** Display Queue *** */
-void displayQueue(Antrian q);
-/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
-   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
-   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
-/* I.S. q boleh kosong */
-/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
-/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
-/* Jika Queue kosong : menulis [] */
-
 
 #endif
