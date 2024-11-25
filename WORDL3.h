a6b786da8a14159f70487764e778542fac7f17ca
Chhoff21 <hansjoseph.silitonga@gmail.com>
2024-11-25 19:55:37 +0700

    Create WORDL3.h
    Signed-off-by: Chhoff21 <hansjoseph.silitonga@gmail.com>

---
+++ WORDL3.h
@@ -0,0 +1,9 @@
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+
+
+Word valid_word();
+// Fungsi mengecek status valid atau tidaknya input dari user
+
+int W0RDL3(int uang);
+// Fungsi utama WORDL3
