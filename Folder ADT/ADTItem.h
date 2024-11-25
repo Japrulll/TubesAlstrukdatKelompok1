32fd6690156e59a6c163ca8d52efa2de3c181d0b
Japrulll <18223067@std.stei.itb.ac.id>
2024-11-25 18:23:16 +0700

    Update ADTItem.h
    Signed-off-by: Japrulll <18223067@std.stei.itb.ac.id>

--- Folder ADT/ADTItem.h
+++ Folder ADT/ADTItem.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "boolean.h"
 
 #define MAX_LEN 100
 
@@ -10,7 +11,7 @@ typedef struct {
     char name[MAX_LEN];
     int price;
 } Barang;
-
+typedef int IdxType;
 typedef struct {
     Barang *items;
     int count;
@@ -31,6 +32,52 @@ void addItem(DinamicItemList *list, int price, char*name);
 void freeDinamicItemList(DinamicItemList *list);
 /*menghapus memori yang telah dialokasikan secara dinamis untuk DinamicItemList,
   supaya memorinya tidak terbuang sia-sia dan bisa digunakan lagi*/
+
+DinamicItemList MakeDinamicItemList();
+
+/**
+ * Destruktor
+ * I.S. DinamicItemList terdefinisi
+ * F.S. array->A terdealokasi
+ */
+void DeallocateDinamicItemList(DinamicItemList *array);
+
+/**
+ * Fungsi untuk mengetahui apakah suatu array kosong.
+ * Prekondisi: array terdefinisi
+ */
+boolean IsEmpty(DinamicItemList array);
+
+/**
+ * Fungsi untuk mendapatkan banyaknya elemen efektif array, 0 jika tabel kosong.
+ * Prekondisi: array terdefinisi
+ */
+int Length(DinamicItemList array);
+
+/**
+ * Mengembalikan elemen array L yang ke-I (indeks lojik).
+ * Prekondisi: array tidak kosong, i di antara 0..Length(array).
+ */
+Barang Get(DinamicItemList array, IdxType i);
+
+/**
+ * Fungsi untuk mendapatkan kapasitas yang tersedia.
+ * Prekondisi: array terdefinisi
+ */
+int GetMAX_LEN(DinamicItemList array);
+
+/**
+ * Fungsi untuk menambahkan elemen baru di index ke-i
+ * Prekondisi: array terdefinisi, i di antara 0..Length(array).
+ */
+void InsertAt(DinamicItemList *array, Barang el, IdxType i);
+
+/**
+ * Fungsi untuk menghapus elemen di index ke-i DinamicItemList
+ * Prekondisi: array terdefinisi, i di antara 0..Length(array).
+ */
+void DeleteAt(DinamicItemList *array, IdxType i);
+
 
 
 #endif
