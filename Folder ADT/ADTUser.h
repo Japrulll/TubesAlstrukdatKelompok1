612b2f57c86d50e79280c087f5ae48c03b793331
FarellaKamala046 <18223046@std.stei.itb.ac.id>
2024-12-12 11:44:47 +0700

    fix: tipe data di adt user

--- Folder ADT/ADTUser.h
+++ Folder ADT/ADTUser.h
@@ -11,7 +11,7 @@ typedef struct {
     char name[MAX_LEN];
     char password[MAX_LEN];
     int money;
-    List keranjang;
+    Map keranjang;
     Stack riwayat_pembelian;
     List wishlist;
 } User;
