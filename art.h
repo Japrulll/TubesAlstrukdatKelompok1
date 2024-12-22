610ac717e21ddfe224320220619060360c9b289a
refinormdhn <167320946+refinormdhn@users.noreply.github.com>
2024-12-22 17:00:10 +0700

    feat : header untuk art
    Signed-off-by: refinormdhn <167320946+refinormdhn@users.noreply.github.com>

---
+++ art.h
@@ -0,0 +1,16 @@
+#ifndef ART_H
+#define ART_H
+
+void artpurrmart();
+void artregisterlogin();
+void artprofile();
+void artwork();
+void artworkchallenge();
+void artstore();
+void artcart();
+void arthistory();
+void artwishlist();
+void artsave();
+void file();
+
+#endif
