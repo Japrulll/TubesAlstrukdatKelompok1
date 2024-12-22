40b2589774af9fb6e5f8d06ac715d1d0d2469be3
refinormdhn <167320946+refinormdhn@users.noreply.github.com>
2024-12-22 15:45:13 +0700

    update : pop dari cartpay dan memindahkan ke tempStack
    Signed-off-by: refinormdhn <167320946+refinormdhn@users.noreply.github.com>

--- history.c
+++ history.c
@@ -2,22 +2,32 @@
 #include <stdio.h>
 #include "./Folder ADT/ADTStack.h"
 
-void dis_history(Stack *S, int N){
-    if (n>S->TOP){
-        n = S->TOP;
+void dis_history(Stack *out, int N){
+    if (N > out->TOP){
+        N = out->TOP;
     }
-    if (Top(*S) == NilS){
+    if (Top(*out) == NilS){
         printf("Kamu belum membeli barang apapun!\n");
         return;
     }
 
     printf("Riwayat pembelian barang:\n");
-    
-    for (int i = 0; i <= n; i++){
-        if (!IsEmptyS(*S)){
-            Pop(&tempStack, &totalHarga, namaBarang);
-            printf("%d %s\n", S->T[i].totalHarga, S->T[i].namaBarang);
-        }
-        return;
+
+    Stack tempStack;
+    CreateEmptyS(&tempStack);
+
+    for (int i = 0; i <= N; i++){
+        int totalHarga;
+        char namaBarang[100];
+        Pop(out, &totalHarga, namaBarang);
+        printf("%d %s\n", totalHarga, namaBarang);  
+        Push(&tempStack, totalHarga, namaBarang);
     }
+    while (!IsEmptyS(tempStack)){
+        int totalHarga; 
+        char namaBarang[100];
+        Pop(&tempStack, &totalHarga, namaBarang);
+        Push(out, totalHarga, namaBarang);
+    }
+    return;
 }
