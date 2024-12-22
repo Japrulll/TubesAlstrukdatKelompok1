a334e7c910dde6eec65a07f0417da955191f2f22
refinormdhn <167320946+refinormdhn@users.noreply.github.com>
2024-12-22 14:05:43 +0700

    feat : header history
    Signed-off-by: refinormdhn <167320946+refinormdhn@users.noreply.github.com>

---
+++ history.h
@@ -0,0 +1,11 @@
+#ifndef HISTORY_H
+#define HISTORY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "./Folder ADT/ADTStack.h"
+
+// show history of purchase with N last purchase
+void history(Stack *S, int N);
+
+#endif
